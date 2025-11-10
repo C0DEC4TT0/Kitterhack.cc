8a9375b84e7d933665c53f6d29230e3c5b837812
C0DEC4TT0 <tech4all@ik.me>
2025-11-11 00:20:23 +0100

    Fixed the client base once again. (This is like the 9th time it broke)

---
+++ src/client/java/com/codecatto/kitterhack/KitterhackClient.java
@@ -0,0 +1,10 @@
+package com.codecatto.kitterhack;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class KitterhackClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
