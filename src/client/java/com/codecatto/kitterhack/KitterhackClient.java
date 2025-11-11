bed3114fec70523e2cad989cc49f04c0255ac67d
C0DEC4TT0 <tech4all@ik.me>
2025-11-11 23:07:04 +0100

    It wouldn't intentionally crash before, that's fixed now.

--- src/client/java/com/codecatto/kitterhack/KitterhackClient.java
+++ src/client/java/com/codecatto/kitterhack/KitterhackClient.java
@@ -3,8 +3,14 @@ package com.codecatto.kitterhack;
 import net.fabricmc.api.ClientModInitializer;
 
 public class KitterhackClient implements ClientModInitializer {
-	@Override
-	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
+    @Override
+    public void onInitializeClient() {
+        // Platform check - will exit immediately if not Linux/Android
+        PlatformCheck.ensureValid();
+
+        System.out.println("Kitterhack.cc initialized!");
+        System.out.println("Running on: " + PlatformCheck.getSystemInfo());
+
+        // This entrypoint is suitable for setting up client-specific logic, such as rendering.
+    }
 }
